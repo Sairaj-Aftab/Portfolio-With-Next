a0a8b765b5395e2e93c14b86e54158105fedd074
Sairaj Aftab <sairajaftab6@gmail.com>
2024-03-22 23:48:32 +0600

    update project

--- data/projects.ts
+++ data/projects.ts
@@ -2,6 +2,7 @@ import youtube from "@/public/project/youtube.png";
 import portfolio from "@/public/project/portfolio.png";
 import portfolio1 from "@/public/project/portfolio1.png";
 import charity from "@/public/project/charity.jpg";
+import photoDiscovery from "@/public/project/photo_discovery.png";
 
 const projects = [
   {
@@ -21,6 +22,12 @@ const projects = [
     title: "Charity Organization",
     tools: ["NextJS"],
     liveLink: "https://next-charity-organization.vercel.app/",
+  },
+  {
+    banner: photoDiscovery,
+    title: "Photo Discovery Web App",
+    tools: ["ReactJS", "NodeJS", "Express", "MongoDB", "Cloudinary"],
+    liveLink: "https://photo-discovery-web-app.onrender.com",
   },
 ];
 
